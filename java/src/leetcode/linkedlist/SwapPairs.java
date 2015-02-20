124c6c26e0bb45bd39a8b8f240127437b5e80b19
Henry Pan <henrypan917@gmail.com>
2015-02-20 17:10:09 -0600

    Add leetcode swap pair

--- java/src/leetcode/linkedlist/SwapPairs.java
+++ java/src/leetcode/linkedlist/SwapPairs.java
@@ -2,5 +2,27 @@ package leetcode.linkedlist;
 
 public class SwapPairs
 {
-
+    public ListNode swapPairs(ListNode head)
+    {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        
+        // Using two pointers, pointer 1 is always ahead of pointer 2.
+        ListNode pointer1 = dummyHead;
+        ListNode pointer2 = head;
+        
+        while (pointer2 != null && pointer2.next != null)
+        {
+            // tempory store the next next node
+            ListNode nextStart = pointer2.next.next;
+            pointer2.next.next = pointer2;
+            pointer1.next = pointer2.next;
+            pointer2.next = nextStart;
+            
+            pointer1 = pointer2;
+            pointer2 = pointer2.next;
+        }
+        
+        return dummyHead.next;
+    }
 }
