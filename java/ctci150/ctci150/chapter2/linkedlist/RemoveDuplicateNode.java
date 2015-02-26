39675a6467a829e9f0a5f3132110226e5f82daf9
Henry Pan <henrypan917@gmail.com>
2015-02-26 10:07:22 -0600

    cc150_checkin

---
+++ java/ctci150/ctci150/chapter2/linkedlist/RemoveDuplicateNode.java
@@ -0,0 +1,34 @@
+package ctci150.chapter2.linkedlist;
+
+public class RemoveDuplicateNode
+{
+    void deleteDupWithoutBuffer(LinkedListNode head)
+    {
+        if (head == null) return;
+        LinkedListNode previous = head;
+        LinkedListNode current = previous.next;
+        
+        while (current != null)
+        {
+            LinkedListNode runner = head;
+            while (runner != current)
+            {
+                if (runner.data == current.data)
+                {
+                    LinkedListNode temp = current.next;
+                    previous.next = temp;
+                    current = temp;
+                    break;
+                }
+                runner = runner.next;
+            }
+            
+            
+            if (runner == current)
+            {
+                previous = current;
+                current = current.next;
+            }
+        }
+    }
+}
