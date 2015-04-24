fe4dee690ba5c514e926fd3560366fa42d970e99
Henry Pan <henrypan917@gmail.com>
2015-04-24 11:40:09 -0500

    leetcode, validate BST: added second method

--- java/src/leetcode/tree/ValidateBST.java
+++ java/src/leetcode/tree/ValidateBST.java
@@ -7,14 +7,15 @@ package leetcode.tree;
  * <p>
  * Copyright &copy 2015 Pan
  * </p>
- *
+ * 
  * @author Henry Pan
  */
 public class ValidateBST
 {
-    
+
     /**
      * This is the brutal force method, it will check every layer of the BT.
+     * 
      * @param root
      * @return {@code true} if it is a valid BST, else return false
      */
@@ -26,16 +27,34 @@ public class ValidateBST
                 && isSubtreeLessThan(root.left, root.value) && isValidBST1(root.left)
                 && isValidBST1(root.right);
     }
-    
+
     private boolean isSubtreeLessThan(TreeNode treeNode, int val)
     {
-        //TODO - you have to put the implementation here
+        // TODO - you have to put the implementation here
         return false;
     }
-    
+
     private boolean isSubtreeGreaterThan(TreeNode treeNode, int val)
     {
-        //TODO - you have to put the implementation here
+        // TODO - you have to put the implementation here
+        return false;
+    }
+
+    /**
+     * This is the top down search method, it will pass the low and high limit from the parent to
+     * child.
+     * 
+     * @param root
+     * @return {@code true} if it is a valid BST, else return false
+     */
+    public boolean isValidBST2(TreeNode root)
+    {
+        return validate(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private boolean validate(TreeNode p, int low, int hight)
+    {
+        // TODO - you have to put the implementation here.
         return false;
     }
 }
